01634e5496284d2bfee2c846591e52e044df1760
ZhaoxiSun-ink <47791231+ZhaoxiSun-ink@users.noreply.github.com>
2023-10-24 15:15:39 -0400

    Create read_cowrie_log.sh

---
+++ read_cowrie_log.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CONTAINER_NAME="honeypot_cowrie"
+current_time=$(date +"%m-%d %H:%M")
+filename="${CONTAINER_NAME}_${current_time}.txt"
+echo "name: $filename"
+docker logs $CONTAINER_NAME >"./logs/$filename"
+
+LOG_FILE=$(docker inspect $CONTAINER_NAME --format='{{.LogPath}}')
+sudo truncate -s 0 $LOG_FILE
