2784cd5503177fe63129ee894e0639faa32b1a05
ZhaoxiSun-ink <47791231+ZhaoxiSun-ink@users.noreply.github.com>
2023-10-24 16:19:30 -0400

    Create read_mail_log.sh

---
+++ read_mail_log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONTAINER_NAME="honeypot_mailoney"
+current_time=$(date +"$m_%d_%H_%M")
+LOG_PATH="/app/logs/mail.log"
+DEST="./logs/${CONTAINER_NAME}_${current_time}.txt"
+
+docker cp "${CONTAINER_NAME}:${LOG_PATH}" "$DEST"
+
+if [ $? -eq 0 ]; then
+        docker exec "$CONTAINER_NAME" truncate -s 0 "$LOG_PATH"
+else
+        echo "Error happen during copying the log from the container. Skipping truncate."
+fi
