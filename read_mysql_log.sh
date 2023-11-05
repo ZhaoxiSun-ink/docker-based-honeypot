7666b049789443205ff72f40a33b40ce91f2b0ad
root <root@honeypot-EA.uwnkyhwcafhuralaydmkqzgs0g.hx.internal.cloudapp.net>
2023-11-05 14:47:41 -0500

    11-5mysql log

---
+++ read_mysql_log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the directory where the logs are stored
+LOG_DIR=/home/zhaxisun/logs/mysql-logs
+
+# Define the MySQL Docker container name
+CONTAINER_NAME=honeypot_mysql
+
+# Move the current log file to a new file with the current date
+mv "$LOG_DIR/general.log" "$LOG_DIR/$(date --date='yesterday' +%Y%m%d).log"
+
+# Execute FLUSH LOGS command on MySQL server to reopen the log file
+docker exec -i $CONTAINER_NAME mysql -uroot -p'123456' -e 'FLUSH LOGS;'
